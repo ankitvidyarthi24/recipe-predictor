195ec8f41f4bc031f0ef5318d84301e81c01b04a
ankitvidyarthi24 <ankitvidyarthi24@gmail.com>
2025-10-05 23:18:37 +0530

    Add files via upload

---
+++ spoonacular_config.js
@@ -0,0 +1,4 @@
+// Paste your Spoonacular API key here.
+// Get one at https://spoonacular.com/food-api
+// NOTE: For production, don't expose API keys in client-side code.
+window.SPOONACULAR_API_KEY = "<YOUR_API_KEY_HERE>";
